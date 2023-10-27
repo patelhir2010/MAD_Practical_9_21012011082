7a5422b501e8e19256765cedecba08494ee8c26a
patelhir2010 <139534069+patelhir2010@users.noreply.github.com>
2023-10-27 11:38:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_9_21012011037/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mad_practical_9_21012011037
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var alarmanimation : AnimationDrawable
+    lateinit var heartanimation : AnimationDrawable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val alarm : ImageView = findViewById(R.id.alarm)
+        alarm.setBackgroundResource(R.drawable.alarm_animation_list)
+        alarmanimation = alarm.background as AnimationDrawable
+
+        val heart : ImageView = findViewById(R.id.heart)
+        heart.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation = heart.background as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            alarmanimation.start()
+            heartanimation.start()
+        }
+        else {
+            alarmanimation.stop()
+            heartanimation.stop()
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD_Practical_9_21012011037"
+include(":app")
